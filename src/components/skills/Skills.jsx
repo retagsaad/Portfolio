49a6bd65102771973f6a40a8d8693954f446e28e
retag <retagsaad77@gmail.com>
2026-01-02 23:39:40 +0200

    Creating a skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import '../skills/skills.css'
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className='skills-container'>
+      <div className="header">
+        <h4 >What Skills I Have</h4>
+        <h2>My Experience</h2>
+      </div>
+    </div>
   )
 }
 
