0a49e2bdf417303f5e3d68f5d2b10e22ef31c18b
retag <retagsaad77@gmail.com>
2026-01-08 23:58:35 +0200

    initialize projects page

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
 import '../projects/project.css'
+import IMG1 from "../../assets/portfolio1.jpg"
+import IMG2 from "../../assets/portfolio2.jpg"
+import IMG3 from "../../assets/portfolio3.jpg"
+import IMG4 from "../../assets/portfolio4.jpg"
+import IMG5 from "../../assets/portfolio5.jpg"
+import IMG6 from "../../assets/portfolio6.jpg"
+
 function Project() {
+  const projectsData =[
+    {
+      id:1,
+      image:IMG1,
+      title:"ITEM 1",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+      {
+      id:2,
+      image:IMG2,
+      title:"ITEM 2",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+      {
+      id:3,
+      image:IMG3,
+      title:"ITEM 3",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+      {
+      id:4,
+      image:IMG4,
+      title:"ITEM 4",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+      {
+      id:5,
+      image:IMG5,
+      title:"ITEM 5",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+      {
+      id:6,
+      image:IMG6,
+      title:"ITEM 6",
+      github:'https://github.com',
+      demo:'https://demo.com',
+    },
+  ]
   return (
-    <div>Project</div>
+    <div className='projects'>
+      <div className='header'>
+        <h4>My Recent Work</h4>
+        <h2></h2>
+      </div>
+      <div className="container projects-container"> 
+        <div className='item'>
+          <div className="img">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="btns">
+          <a href={github} className="btn">GitHub</a>
+          <a href={demo} className="btn-primary btn">Live Demo</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
