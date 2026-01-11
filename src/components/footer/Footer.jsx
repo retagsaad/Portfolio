70432752fb3d649bbbb330c68eb5d696d28e60c5
retag <retagsaad77@gmail.com>
2026-01-11 21:05:36 +0200

    initialize footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import "../footer/footer.css"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 function Footer() {
   return (
-    <div></div>
+    <div className='footer'>
+      <div className='container footer-container'>
+        <a className='footer-logo'>Retag Saad</a>
+        <ul className="links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#porjects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="contact">
+          <a href="#" target='_blank'><FaLinkedin/></a>
+          <a href="#" target='_blank'><FaGithub/></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
