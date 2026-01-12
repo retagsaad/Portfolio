1f2141e1210de6cdf3019e0c1aab01f21de59e32
retag <retagsaad77@gmail.com>
2026-01-12 23:49:38 +0200

    initialize service page

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import '../service/service.css'
+import { LiaPencilRulerSolid } from "react-icons/lia";
+import { FaCode } from "react-icons/fa";
+import { IoIosRocket } from "react-icons/io";
+
+const servicesData =[
+  {
+    id:1,
+    icon:<LiaPencilRulerSolid />,
+    title:'Web Design',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A maiores reprehenderit laudantium ab cumque illo!',
+  },
+  {
+    id:2,
+    icon:<IoIosRocket />,
+    title:'Fast Performance',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A maiores reprehenderit laudantium ab cumque illo!',
+  },
+  {
+    id:3,
+    icon:<FaCode />,
+    title:'Clean Code',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A maiores reprehenderit laudantium ab cumque illo!',
+  }
+]
 function Service() {
   return (
-    <div id='service'></div>
+    <div id='service' className='service'>
+      <div className='header'>
+    <h4>What I Offer</h4>
+    <h2>Services</h2>
+      </div>
+      <div className="container container-service">
+        {servicesData.map(({id,icon,title,text})=>(
+          <div key={id} className="card">
+        <div>{icon}</div>
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </div>
+        ))}
+      
+      </div>
+    </div>
   )
 }
 
