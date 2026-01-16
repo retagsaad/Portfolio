f7794decdf07759ef3478d4e4091d002792b3f36
retag <retagsaad77@gmail.com>
2026-01-16 21:40:53 +0200

    daily update

--- src/components/home/HomeSocial.jsx
+++ src/components/home/HomeSocial.jsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 function HomeSocial() {
   return (
     <div className='home_social'>
-        <a href="www.linkedin.com/in/retag-saad77" target="_blank"><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/retag-saad77/" target="_blank"><FaLinkedin/></a>
         <a href="https://github.com/retagsaad" target="_blank"><FaGithub/></a>
 
     </div>
