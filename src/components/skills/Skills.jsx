9983624d5f4d46dba0a156517c168cde4f616f6c
retag <retagsaad77@gmail.com>
2026-04-10 16:40:10 +0200

    update

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,62 +1,39 @@
 import "../skills/skills.css";
-import CSS from "../../assets/css3.svg"
-import Javascript from "../../assets/javascript.svg"
-import Bootstrap from "../../assets/Bootstrap.png"
-import React from "../../assets/React.svg"
-import Git from "../../assets/Git.png"
-import Tailwind from "../../assets/tailwind.png"
+import CSS from "../../assets/css3.svg";
+import Javascript from "../../assets/javascript.svg";
+import Bootstrap from "../../assets/Bootstrap.png";
+import React from "../../assets/React.svg";
+import Git from "../../assets/Git.png";
+import Tailwind from "../../assets/tailwind.png";
+import next from '../../assets/next.png'
 
-const SkillsData =[
-  {id:1,
-    image:CSS,
-    title:'Css',
-    disc: "User interface",
-  },
-  {id:2,
-    image:Bootstrap,
-    title:'Bootstrap',
-    disc: "Framework",
-  },
-  {id:3,
-    image:Javascript,
-    title:'Javascript',
-    disc: "Interaction",
-  },
-    {id:4,
-    image:React,
-    title:'React',
-    disc: "Framework",
-  },
-    {id:5,
-    image:Git,
-    title:'Git & GitHub',
-    disc: "Version Control System",
-  },
-    {id:6,
-    image:Tailwind,
-    title:'TailwindCSS',
-    disc: "Framework",
-    
-  },
-]
+const SkillsData = [
+  { id: 1, image: CSS, title: "Css", disc: "User interface" },
+  { id: 2, image: Bootstrap, title: "Bootstrap", disc: "Framework" },
+  { id: 3, image: Javascript, title: "Javascript", disc: "Interaction" },
+  { id: 4, image: React, title: "React", disc: "Framework" },
+  { id: 5, image: next, title: "Next", disc: "Framework" },
+  { id: 6, image: Tailwind, title: "TailwindCSS", disc: "Framework" },
+  { id: 7, image: Git, title: "Git & GitHub", disc: "Version Control System" },
+];
 
 function Skills() {
   return (
-    <div className="skiils"id="skills">
+    <div className="skiils" id="skills">
       <div className="header">
         <h5>What Skills I Have</h5>
         <h3>My Experience</h3>
         <div className="skills-container container">
-          {SkillsData.map(({id , image , title , disc}) =>(
-            <div key={id}className="card-skill">
-            <div className="logo">
-              <img src={image} alt={title} />
+          {SkillsData.map(({ id, image, title, disc }) => (
+            <div key={id} className="card-skill">
+              <div className="logo">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <h4>{title}</h4>
+                <p className="text-light">{disc}</p>
+              </div>
             </div>
-            <div className="content">
-            <h4>{title}</h4>
-            <p className="text-light">{disc}</p>
-            </div>
-          </div> 
           ))}
         </div>
       </div>
